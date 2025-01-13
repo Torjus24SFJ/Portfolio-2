bd548dbc21a99cf80d0afe8459ebf8e2d9aff08e
Torjus <Torjus24sfj@gmail.com>
2025-01-13 13:06:36 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          // primary: #000000,
+          // secondary: #FFFFFF,
+        },
+      },
+    },
+  };
+  
